00071b9650acf1bd78ab3668a71ff7b673632ec1
taufiqdp <topexdw@gmail.com>
2024-08-29 16:16:50 +0700

    update: navbar

--- components/navbar.js
+++ components/navbar.js
@@ -17,44 +17,50 @@ export default function Navbar() {
     <nav className="bg-black shadow border-b-2 border-neutral-700 w-full fixed top-0 left-0 z-50">
       <div
         className={`${
-          isOpen ? "h-40" : "h-16"
-        } md:h-16 mx-auto md:px-4 container flex items-center justify-between transition-all duration-300 ease-in-out`}
+          isOpen ? "h-72" : "h-16"
+        } md:h-16 mx-auto md:px-4 container flex items-center transition-all duration-300 ease-in-out`}
       >
-        {/* <div className="md:absolute md:left-4 order-2 md:order-1 mx-auto">
-          <Link href="/">
-            <Image src={logoImg} alt="Logo" width={50} height={50} />
+        {/* logo */}
+        <div>
+          <Link href="/" className="flex items-center flex-shrink-0">
+            <Image
+              src={logoImg}
+              height={40}
+              width={40}
+              alt="logo"
+              className={`rounded-full ml-4`}
+            />
+            <h1 className="text-rose-50 text-2xl font-bold pl-3">Foodies</h1>
           </Link>
-        </div> */}
+        </div>
 
-        <div className="text-gray-500 w-full">
+        {/* nav links */}
+        <div className="text-gray-500 w-full mx-14">
           <ul
             className={`${
               isOpen
                 ? "max-h-[500px] opacity-100"
                 : "max-h-0 opacity-0 md:opacity-100 md:max-h-[500px]"
-            } md:flex md:justify-center px-4 transition-all duration-300 ease-in-out overflow-hidden`}
+            } md:flex md:justify-end md:text-base text-2xl md:space-y-0 space-y-4 pl-2
+              transition-all duration-300 ease-in-out overflow-hidden md:border-none border-l-4 
+              border-neutral-400`}
           >
-            <li className="md:px-4 md:py-2 hover:text-rose-50">
+            <li className="md:px-3 px-2 md:py-2 text-rose-50">
               <Link href="/" onClick={toggleMenu}>
                 Home
               </Link>
             </li>
-            <li className="md:px-4 md:py-2 hover:text-rose-50">
+            <li className="md:px-3 px-2 md:py-2 hover:text-rose-50">
               <Link href="/meals" onClick={toggleMenu}>
-                Meals
+                Browse Meals
               </Link>
             </li>
-            <li className="md:px-4 md:py-2 hover:text-rose-50">
+            <li className="md:px-3 px-2 md:py-2 hover:text-rose-50">
               <Link href="/community" onClick={toggleMenu}>
                 Community
               </Link>
             </li>
-            <li className="md:px-4 md:py-2 hover:text-rose-50">
-              <Link href="/about" onClick={toggleMenu}>
-                About
-              </Link>
-            </li>
-            <li className="md:px-4 md:py-2 hover:text-rose-50">
+            <li className="md:px-3 px-2 md:py-2 hover:text-rose-50">
               <Link href="/contact" onClick={toggleMenu}>
                 Contact
               </Link>
@@ -62,6 +68,7 @@ export default function Navbar() {
           </ul>
         </div>
 
+        {/* mobile menu button */}
         <div className="order-3 md:hidden flex fixed right-3 top-5">
           <button
             className="text-gray-500 focus:outline-none"
