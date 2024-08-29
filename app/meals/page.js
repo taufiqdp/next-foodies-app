fde5767fa9323dc27f0a43e5ccf361ccc39a22fe
taufiqdp <topexdw@gmail.com>
2024-08-29 21:07:55 +0700

    update: search bar

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,6 @@
 import Cards from "@/components/cards";
 import { IoMdSearch } from "react-icons/io";
+import { Input } from "@/components/ui/input";
 
 export default function MealsPage() {
   return (
@@ -7,12 +8,7 @@ export default function MealsPage() {
       <div className="w-full flex flex-col items-center pb-4">
         <h1 className="text-2xl font-bold p-4">Browse Meals</h1>
         <form className="flex items-center">
-          <input
-            className=" border-neutral-400 w-80 py-2 px-3 text-rose-50 bg-neutral-800 focus:outline-none"
-            id="search"
-            type="text"
-            placeholder="Search..."
-          />
+          <Input type="text" placeholder="Search..." />
           <button class="bg-transparent hover:text-white h-10 py-2 px-4 hover:bg-neutral-950">
             <IoMdSearch />
           </button>
