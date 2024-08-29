fc5511a23a5ce7b2d9abd416485fefb2338589fb
taufiqdp <topexdw@gmail.com>
2024-08-29 16:18:57 +0700

    update: meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,16 @@ import Cards from "@/components/cards";
 
 export default function MealsPage() {
   return (
-    <main className="flex flex-col items-center justify-center p-6">
-      <h1 className="text-2xl font-bold mb-2">Meals</h1>
+    <main className="flex flex-col items-center p-6">
+      <div className="relative flex w-full justify-center mb-20 md:mb-4">
+        <h1 className="text-2xl font-bold">Meals</h1>
+        <input
+          className="absolute top-0 md:right-0 mt-12 md:mt-0 border-neutral-400 w-48 py-2 px-3 text-rose-50 bg-neutral-800 focus:outline-none"
+          id="search"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
       <Cards />
     </main>
   );
