0f73c5bf76aa3186c114483c753d167a03ee957b
taufiqdp <topexdw@gmail.com>
2024-08-30 20:16:25 +0700

    add: loading page

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+  return (
+    <div className="flex justify-center py-48">
+      <div className="border-neutral-800 h-20 w-20 animate-spin rounded-full border-8 border-t-rose-50" />
+    </div>
+  );
+}
