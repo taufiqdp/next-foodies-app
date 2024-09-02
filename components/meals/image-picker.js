221c8c32bde75b52085cd00528588a2780f39a36
taufiqdp <topexdw@gmail.com>
2024-09-02 13:32:51 +0700

    update: fix cannot get image data

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -7,16 +7,14 @@ import { IoCloudUploadOutline } from "react-icons/io5";
 
 export default function ImagePicker({ label, name }) {
   const [pickImage, setPickImage] = useState(null);
-
   const imageInput = useRef();
 
-  function handlerPickClick() {
+  const handlerPickClick = () => {
     imageInput.current.click();
-  }
+  };
 
-  function handlerImageChange(event) {
+  const handlerImageChange = (event) => {
     const file = event.target.files[0];
-
     if (!file) {
       setPickImage(null);
       return;
@@ -28,38 +26,16 @@ export default function ImagePicker({ label, name }) {
     };
 
     fileReader.readAsDataURL(file);
-  }
+  };
 
-  function handlerRemoveImage() {
+  const handlerRemoveImage = () => {
     setPickImage(null);
-  }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center md:w-[300px] md:h-[300px]">
-      <label htmlFor={name} className="block text-rose-50 mb-2 text-center">
-        {label}
-      </label>
-
       {!pickImage ? (
-        <>
-          <input
-            type="file"
-            id={name}
-            accept="image/png, image/jpeg"
-            name={name}
-            ref={imageInput}
-            className="hidden"
-            onChange={handlerImageChange}
-          />
-          <button
-            type="button"
-            onClick={handlerPickClick}
-            className="flex flex-col items-center text-rose-50 hover:underline hover:underline-offset-4 "
-          >
-            <IoCloudUploadOutline className="w-12 h-12" />
-            <p>Upload Image</p>
-          </button>
-        </>
+        <></>
       ) : (
         <div
           className="relative w-full h-full overflow-hidden rounded-md hover:cursor-pointer"
@@ -75,6 +51,26 @@ export default function ImagePicker({ label, name }) {
           />
         </div>
       )}
+
+      <input
+        type="file"
+        id={name}
+        accept="image/png, image/jpeg"
+        name={name}
+        ref={imageInput}
+        className="hidden"
+        onChange={handlerImageChange}
+      />
+      <button
+        type="button"
+        onClick={handlerPickClick}
+        className={`flex flex-col items-center text-rose-50 hover:underline hover:underline-offset-4 ${
+          pickImage !== null && "hidden"
+        }`}
+      >
+        <IoCloudUploadOutline className="w-12 h-12" />
+        <p>Upload Image</p>
+      </button>
     </div>
   );
 }
